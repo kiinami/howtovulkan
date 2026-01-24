7693a7438a111f6868901310df8f08c8b73577e2
Sascha Willems <webmaster@saschawillems.de>
2026-01-24 18:23:27 +0100

    Remove debug output

--- source/main.cpp
+++ source/main.cpp
@@ -111,7 +111,6 @@ int main(int argc, char* argv[])
 {
 	chk(SDL_Init(SDL_INIT_VIDEO));
 	chk(SDL_Vulkan_LoadLibrary(NULL));
-	std::cout << SDL_GetError() << "\n";
 	volkInitialize();
 	// Instance
 	VkApplicationInfo appInfo{ .sType = VK_STRUCTURE_TYPE_APPLICATION_INFO, .pApplicationName = "How to Vulkan", .apiVersion = VK_API_VERSION_1_3 };
